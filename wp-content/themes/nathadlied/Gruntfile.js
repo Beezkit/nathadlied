e7d5348f193465c2ffba4673ca3827b0d0137051
Beezkit <g.delforge.gd@gmail.com>
2017-02-07 09:32:40 +0100

    ajout theme et gruntJs

---
+++ wp-content/themes/nathadlied/Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+
+
+  require('load-grunt-tasks')(grunt);
+  // Configuration de Grunt
+  grunt.initConfig({
+
+    jshint: {
+      all: 'js/global.js',
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      dist: {
+        files: {
+          'js/global.min.js': 'js/global.js'
+        }
+      }
+    },
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed',
+          sourcemap: 'none'
+        },
+        files: {                         // Dictionary of files
+          'style.css': 'scss/style.scss'     // 'destination': 'source'
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        files: 'js/global.js',
+        tasks: ['jshint', 'uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: ['scss/*.scss'],
+        tasks: 'sass',
+        options: {
+          spawn: false,
+        },
+      }
+    },
+
+    sprite:{
+      all: {
+        src: 'img/**/*.png',
+        dest: 'dist/sprite.png',
+        destCss: 'css/sprites.css'
+      }
+    }
+
+  })
+
+  // Définition des tâches Grunt
+  grunt.registerTask('default', ['jshint', 'sass', 'sprite', 'uglify']);
+
+}
---
+++ wp-content/themes/nathadlied/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <link rel="canonical" href="http://www.guillaume-delforge.fr" />
+    <title><?php wp_title(); ?></title>
+    <meta http-equiv="content-language" content="fr-FR" />
+    <meta name="author" content="DELFORGE Guillaume" />
+    <meta name="copyright" content="Copyright (c)2016 - 2017 DELFORGE Guillaume. Tout droit reservé." />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<h1>test</h1>
